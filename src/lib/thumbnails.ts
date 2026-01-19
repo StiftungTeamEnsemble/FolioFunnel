67c5e9ff767dbec04d93226cbdb29d3d5b6ee1df
signalwerk <sh@signalwerk.ch>
2026-01-19 14:49:56 +0100

    FIX: processor name

--- src/lib/thumbnails.ts
+++ src/lib/thumbnails.ts
@@ -1,4 +1,4 @@
-export const PDF_THUMBNAIL_COLUMN_KEY = 'pdf_thumbnail';
+export const PDF_THUMBNAIL_COLUMN_KEY = 'pdf_to_thumbnail_mupdf';
 export const PDF_THUMBNAIL_FILENAME = 'thumbnail.png';
 
 export function getDocumentThumbnailUrl(projectId: string, documentId: string) {
